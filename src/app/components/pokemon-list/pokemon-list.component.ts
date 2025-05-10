d731d15b51dfd8e9b62ce3d5c52b6364dddbfdb9
Mihai Radu <mihai.radu0401@gmail.com>
2025-05-10 19:38:57 +0300

    [Jira: PZ-9999] Replace hardcoded list of pokemons and add load more pokemons and tab content functionality

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output, signal } from '@angular/core';
 import { SimplePokemon } from 'types/simple-pokemon.type';
-
 import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item.component';
 
 @Component({
@@ -24,6 +23,16 @@ import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item
                 @for (pokemon of pokemonList(); let index = $index; track pokemon.name) {
                     <app-pokemon-list-item [index]="$index" [pokemon]="pokemon" />
                 }
+                @if (nextPageUrl) {
+                    <button (click)="loadMore()" class="p-2 rounded-md bg-cyan-500 font-semibold">
+                        Load more Pokemons
+                    </button>
+                } @else {
+                    <div class="text-center p-2 font-semibold">
+                        No more Pokemons ...
+                    </div>
+                 }
+                
             </section>
         </div>
 
@@ -34,4 +43,12 @@ import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item
 })
 export class PokemonListComponent {
     readonly pokemonList = input<SimplePokemon[]>([]);
-}
+
+    @Input() nextPageUrl = signal<string | null>(null);
+    @Output() loadMoreRequest = new EventEmitter<void>();
+    
+    loadMore() {
+        this.loadMoreRequest.emit();
+    }
+
+  }
--- src/app/containers/pokemon-detail/pokemon-detail.container.ts
+++ src/app/containers/pokemon-detail/pokemon-detail.container.ts
@@ -4,12 +4,14 @@ import { injectQuery } from '@tanstack/angular-query-experimental';
 import { firstValueFrom } from 'rxjs';
 import { Pokemon } from 'types/pokemon.type';
 
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 import { injectTwHostClass } from 'util/inject-tw-host-class.util';
 import { PokemonInfoComponent } from '../../components/pokemon-info/pokemon-info.component';
 
 @Component({
     selector: 'app-pokemon-detail',
-    imports: [PokemonInfoComponent],
+    imports: [PokemonInfoComponent, CommonModule],
     template: `
         <div class="w-full bg-black text-white h-60 p-2 rounded-md shadow-inner">
             @if (currentPokemonInfo.data(); as pokemonInfo) {
@@ -25,17 +27,37 @@ import { PokemonInfoComponent } from '../../components/pokemon-info/pokemon-info
         -->
         <div class="flex flex-row *:flex-auto gap-2">
             <!-- Q: Should these become a separate button/tab component? Maybe use existing lib? -->
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 1</button>
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 2</button>
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 3</button>
+            <button class="bg-cyan-300 p-2 rounded-md" [class.bg-cyan-500]="activeTabIndex === 1" (click)="setActiveTabIndex(1)">Abilities</button>
+            <button class="bg-cyan-300 p-2 rounded-md" [class.bg-cyan-500]="activeTabIndex === 2" (click)="setActiveTabIndex(2)">Stats</button>
+            <button class="bg-cyan-300 p-2 rounded-md" [class.bg-cyan-500]="activeTabIndex === 3" (click)="setActiveTabIndex(3)">Types</button>
         </div>
 
-        <div class="grow bg-gray-200 p-2 rounded-md">Tab content goes here</div>
+        <div class="grow bg-gray-200 p-2 rounded-md">
+            <div class="grow bg-gray-200 p-2 rounded-md">
+                @switch (activeTabIndex) {
+                    @case (1) {
+                        Content for tab 1 goes here
+                    }
+                    
+                    @case (2) {
+                        content for tab 2 goes here
+                    }
+
+                    @case (3) {
+                        Content for Tab 3 goes here
+                    }
+                }
+            </div>
+        </div>
     `,
 })
 export class PokemonDetailContainer {
     private readonly httpClient = inject(HttpClient);
     private readonly pokemonId = signal('bulbasaur');
+
+    activeTabIndex: number = 1;
+    pokemonName = '';
+    pokemonAbilities = [];
 
     readonly currentPokemonInfo = injectQuery(() => ({
         queryKey: ['pokemon', this.pokemonId()],
@@ -44,7 +66,20 @@ export class PokemonDetailContainer {
             firstValueFrom(this.httpClient.get<Pokemon>(`/api/v2/pokemon/${this.pokemonId()}`)),
     }));
 
-    constructor() {
+    constructor(private route: ActivatedRoute) {
         injectTwHostClass(() => 'flex flex-col gap-4 p-5 pt-20');
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            const pokemonName = params['pokemonId'];
+            if (pokemonName) {
+                this.pokemonId.set(pokemonName);
+            }
+        });
+    }
+
+    setActiveTabIndex(tabIndex: number) {
+        this.activeTabIndex = tabIndex;
     }
 }
